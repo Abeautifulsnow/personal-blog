36bfed68a5650b5ac126641e322dc5f85e64c848
Abeautifulsnow <1491444340@qq.com>
2018-12-17 17:24:37 +0800

    add blog and recommendation

--- work_blog/apps/blog/urls.py
+++ work_blog/apps/blog/urls.py
@@ -1,16 +1,11 @@
 # encoding: utf-8
 from django.urls import path, re_path
-from blog.views import blog_detail, blogs_type, blogs_tags, blog_list, blogs_by_date, blog_edit, add_ajax, add
+from blog.views import blog_detail, blogs_type, blogs_tags, blog_list, blogs_by_date
 
 urlpatterns = [
     path("", blog_list, name="blog_list"),
     # http://127.0.0.1:8000/blog/1 博客详情页
     path('<int:blog_pk>/', blog_detail, name='blog_detail'),
-    # 增加博客页面
-    path('blog_add', add, name='blog_add'),
-    path('blog_add_ajax', add_ajax, name="add_ajax"),
-    # 博客编辑
-    path('edit/<int:blog_pk>/', blog_edit, name="blog_edit"),
     # 博客分类页面
     path('type/<int:blog_type_pk>/', blogs_type, name="blogs_type"),
     # 博客标签页面
--- work_blog/apps/blog/views.py
+++ work_blog/apps/blog/views.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import json
+import random
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.db.models import Count, Q
@@ -17,20 +18,20 @@ from user.models import UserProfile
 # Create your views here.
 
 # 装饰器，管理员判断
-def check_amdin(func):
-    def wrapper(request):
-        if request.user.is_authenticated():
-            if request.user.is_superuser:
-                return func(request)
-        context = {
-            'redirect_to': '/',
-            'goto_time': 3000,
-            'goto_page': True,
-            'msg': '您不是管理员，不能进入这个界面'
-        }
-        return render(request, 'error.html', context)
-
-    return wrapper
+# def check_amdin(func):
+#     def wrapper(request):
+#         if request.user.is_authenticated():
+#             if request.user.is_superuser:
+#                 return func(request)
+#         context = {
+#             'redirect_to': '/',
+#             'goto_time': 3000,
+#             'goto_page': True,
+#             'msg': '您不是管理员，不能进入这个界面'
+#         }
+#         return render(request, 'error.html', context)
+#
+#     return wrapper
 
 
 def get_blog_list_common(request, all_blogs):
@@ -166,96 +167,17 @@ def blog_detail(request, blog_pk):
     # 博客上一页
     pre_blog = Blog.objects.filter(create_time__lt=blog.create_time).first()
 
+    # 随机推荐
+    rand_count = 12
+    rand_blogs = Blog.objects.exclude(id=blog_pk).order_by('?')[:rand_count]
+
     context = {
         'blog': blog,
         'pre_blog': pre_blog,
         'next_blog': next_blog,
+        'rand_blogs': rand_blogs,
     }
     response = render(request, 'blog_detail.html', context)  # 响应
     # step1：给浏览器设置cookie，用于判断
     response.set_cookie(read_cookie_key, 'true')  # 阅读cookie标记
     return response
-
-@login_required
-def blog_edit(request, blog_pk):
-    """
-    博客内容编辑
-    """
-    blog = Blog.objects.get(id=blog_pk)
-    
-    if request.user != blog.author:
-        raise Http404
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-
-        blog.title = title
-        blog.content = content
-        blog.save()
-        return HttpResponseRedirect(reverse('blog:blog_list'))
-    else:
-        pass
-    context = {
-        'blog': blog,
-    }
-    return render(request, 'blog_edit.html', context)
-
-@check_amdin
-def add(request):
-    blog_types = BlogType.objects.all()
-    data = {
-        'blog_types': blog_types,
-    }
-    return render(request, 'add_blog.html', data, context_instance=RequestContext(request))
-
-@check_amdin
-def add_ajax(request):
-    data = {}
-    try:
-        if request.method != 'POST':
-            raise Exception('methos error')
-        #获取数据
-        #这些相关数据都是前端页面表单中的字段
-        #且和我博客模型一一对应，你根据实际情况修改即可
-        blog_title = request.POST.get('title')
-        blog_types = request.POST.getlist('blog_type')
-        blog_content = request.POST.get('content')
-
-        #处理blog_types
-        blog_type_ids = []
-        for blog_type in blog_types:
-            try:
-                blog_type_id = int(blog_type)
-                blog_type_ids.append(blog_type_id)
-            except:
-                pass
-        if len(blog_title) <= 0 or len(blog_title) >= 50:
-            raise Exception('请输入一个不超过50个字符的标题')
-        if len(blog_type_ids) < 1:
-            raise Exception('请选择"推荐"之外的至少一个类别')
-        if len(blog_content) == 0:
-            raise Exception('尚未写入任何博文')
-        
-        #新增博文
-        blog = Blog()
-        blog.title = blog_title #标题
-        
-        #这里后面我还要修改，统一用Django的用户系统
-        #Author是一个作者模型
-        blog.author = UserProfile.objects.all()[0].username
-        
-        blog.content = blog_content #博客内容
-        blog.save()
- 
-        #处理多对多，需要博文保存后才能处理，且新增后无需使用save方法
-        for blog_type in BlogType.objects.filter(id__in = blog_type_ids):
-            blog.blog_type.add(blog_type)        
- 
-        #返回结果
-        data['success'] = True
-        data['message'] = reverse('blog:blog_detail', args = [blog.id,])
-    except Exception as e:
-        data['success'] = False
-        data['message'] = e.message
-    
-    return HttpResponse(json.dumps(data), content_type = 'application/json')
