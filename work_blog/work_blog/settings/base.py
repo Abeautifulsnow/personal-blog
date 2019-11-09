deb486f2df34d047dd2e7d210e27b0a9ed899568
Abeautifulsnow <1491444340@qq.com>
2019-11-10 02:05:55 +0800

    修改redis密码配置

--- work_blog/work_blog/settings/base.py
+++ work_blog/work_blog/settings/base.py
@@ -213,7 +213,7 @@ CACHES = {
         'LOCATION': 'redis://127.0.0.1:6379',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            # 'PASSWORD': os.environ['redis_passwd'],
+            'PASSWORD': os.environ['redis_passwd'],
         },
     },
 }
